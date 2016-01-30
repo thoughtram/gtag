75f64eb85a20f7164121cb30dafa0a58565a4c87
Christoph Burgdorf <christoph.burgdorf@gmail.com>
2016-01-30 16:03:39 +0100

    fix: update version behind version flag

--- src/main.rs
+++ src/main.rs
@@ -5,7 +5,7 @@ use std::process::Command;
 
 fn main() {
     let matches = App::new("gtag")
-                             .version("0.2.1")
+                             .version("0.3.0")
                              .author("Christoph Burgdorf <christoph@thoughtram.io>")
                              .about("The missing range tag command for Git")
                              .arg(Arg::with_name("range")
